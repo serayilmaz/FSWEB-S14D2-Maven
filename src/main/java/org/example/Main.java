a31b0f18d4afdfb1c849b5039bc86bf8dd73446d
serayilmaz <serayilmaz96@gmail.com>
2026-02-21 22:22:35 +0300

    feat: add bedroom domain models and enums

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 1) Önce parçaları oluştur
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("West");
+
+        Ceiling ceiling = new Ceiling(250, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 4, 60, 2, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 85);
+        Wardrobe wardrobe = new Wardrobe(200, 220, 80.5);
+        Carpet carpet = new Carpet(300, 200, PaintColor.GREEN);
+
+        // 2) Bedroom oluştur
+        Bedroom bedroom = new Bedroom(
+                "Master Bedroom",
+                wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet
+        );
+
+        // 3) Getter ile erişim (değerlere ulaşabiliyor musun?)
+        System.out.println("Bedroom name: " + bedroom.getName());
+        System.out.println("Wall1 direction: " + bedroom.getWall1().getDirection());
+        System.out.println("Ceiling height: " + bedroom.getCeiling().getHeight());
+        System.out.println("Carpet color: " + bedroom.getCarpet().getColor());
+        System.out.println("Lamp style: " + bedroom.getLamp().getStyle());
+        System.out.println("Lamp rating: " + bedroom.getLamp().getGlobalRating());
+
+        // 4) Metod çağrıları
+        bedroom.getWall1().create();
+        bedroom.getWall2().create();
+        bedroom.getWall3().create();
+        bedroom.getWall4().create();
+        bedroom.getCeiling().create();
+
+        bedroom.getBed().make();
+        bedroom.getLamp().turnOn();
+        bedroom.getWardrobe().add();
+        bedroom.getCarpet().lying();
     }
 }
---
+++ src/main/java/org/example/model/Lamp.java
@@ -0,0 +1,31 @@
+package org.example.model;
+
+import org.example.model.enums.LampType;
+
+public class Lamp {
+    private LampType style;
+    private boolean battery;
+    private int globalRating;
+
+    public Lamp(LampType style, boolean battery, int globalRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globalRating = globalRating;
+    }
+
+    public void turnOn() {
+        System.out.println("Lamp is being turned on.");
+    }
+
+    public LampType getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobalRating() {
+        return globalRating;
+    }
+}
